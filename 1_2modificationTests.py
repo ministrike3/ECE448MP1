28400b85ced7d850922fa5650f7a807581122adc
Neil <neil123@gmail.com>
2017-10-03 20:51:43 -0500

    Testing Astar

--- 1_2modificationTests.py
+++ 1_2modificationTests.py
@@ -1,14 +1,18 @@
 from collections import deque
 from heapq import heappop, heappush
+from itertools import combinations
 import glob
 import copy
+import time
 
 
 def get_list_of_multi_dot_files():
     return glob.glob("./Inputs/Search/*")
 
+
 def get_list_of_1dot_files():
     return glob.glob("./Inputs/1dot/*")
+
 
 def input_to_array(file):
     lines = []
@@ -51,7 +55,6 @@ def maze2graph(maze):
         if col < width - 1 and maze[row][col + 1] == ' ':
             graph[(row, col)].append(("E", (row, col + 1)))
             graph[(row, col + 1)].append(("W", (row, col)))
-        graph[(row, col)].append(("State", 0))
     return graph
 
 
@@ -116,9 +119,57 @@ def find_path_astar(graph, start, goal):
             continue
         visited.add(current)
         for direction, neighbour in graph[current]:
-            heappush(pr_queue, (cost + manhattan_distance_heuristic(neighbour, goal), cost + 1,
-                                path + direction, neighbour))
+            heappush(pr_queue, (cost + manhattan_distance_heuristic(neighbour, goal), cost + 1, path + direction, neighbour))
     return "NO WAY!", len(visited)
+
+
+def neil_find_path_astar(graph, start, goal_input):
+    def heuristic(start,goal_list):
+        def manhattan_distance(cell, goal):
+            return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+        if len(goal_list)==0:
+            return(0)
+        elif len(goal_list)==1:
+            return(manhattan_distance(start, goal_list[0]))
+        else:
+            max_distance=0
+            for pair in combinations(goal_list, 2):
+                if manhattan_distance(pair[0],pair[1]) > max_distance:
+                    max_distance = manhattan_distance(pair[0],pair[1])
+                    max_pair=pair
+            d=min(manhattan_distance(start, max_pair[0]),manhattan_distance(start, max_pair[1]))
+            return(max_distance+d)
+
+    #heuristic(start,goals)
+    state = set(goal_input)
+    pr_queue = []
+    heappush(pr_queue, (0 + heuristic(start, state), 0, "", start,state))
+    visited = set()
+    while pr_queue:
+        _, cost, path, current,goals_left = heappop(pr_queue)
+        goals_targeted=list(goals_left)
+        if current in goals_targeted:
+            goals_targeted.remove(current)
+            #    print('GOAL')
+
+        if len(goals_targeted) == 0:
+            return path, len(visited)
+
+        if (current, frozenset(goals_targeted)) in visited:
+            # print('Already In Visited')
+            # print(visited)
+            continue
+
+        goals = list(goals_targeted)
+        visited.add((current, frozenset(goals)))
+        for direction, neighbour in graph[current]:
+            heappush(pr_queue, (cost + heuristic(neighbour, goals), cost + 1, path + direction, neighbour,goals))
+    #return "NO WAY!", len(visited)
+
+
+
+
 
 def find_path_greedy(graph, start, goal):
     def manhattan_distance_heuristic(cell, goal):
@@ -137,7 +188,6 @@ def find_path_greedy(graph, start, goal):
             heappush(pr_queue, (manhattan_distance_heuristic(neighbour, goal), cost + 1,
                                 path + direction, neighbour))
     return "NO WAY!", len(visited)
-
 
 
 def print_solved_maze(start, maze, solution, goals, expanded_nodes, file):
@@ -173,7 +223,7 @@ def print_solved_maze(start, maze, solution, goals, expanded_nodes, file):
 if __name__ == "__main__":
     #inputs = get_list_of_multi_dot_files()
     #inputs = ['./Inputs/1dot/mediumMaze.txt']
-    inputs = ['./Inputs/Search/tinySearch.txt']
+    inputs = ['./Inputs/Search/mediumSearch.txt']
 
     for raw_input in inputs:
         maze = input_to_array(raw_input)
@@ -193,12 +243,12 @@ if __name__ == "__main__":
         #state123=[item for item in graph[(7,3)] if item[0]=='State'][0][1]
         #print(state123)
         print('start')
-        solution, expanded_nodes = find_path_bfs(graph, start, goals)
-        print(solution)
-        print(expanded_nodes)
-        file_name = './Outputs/Search/BFS/' + name + '.txt'
-        print(file_name)
-        print_solved_maze(start, maze, solution, goals, expanded_nodes, file_name)
+        #solution, expanded_nodes = find_path_bfs(graph, start, goals)
+        #print(solution)
+        #print(expanded_nodes)
+        #file_name = './Outputs/Search/BFS/' + name + '.txt'
+        #print(file_name)
+        #print_solved_maze(start, maze, solution, goals, expanded_nodes, file_name)
         #
         # solution, expanded_nodes = find_path_dfs(graph, start, goals[0])
         # # print(solution)
@@ -207,12 +257,16 @@ if __name__ == "__main__":
         # print(file_name)
         # print_solved_maze(start, maze, solution, goals, expanded_nodes, file_name)
         #
-        # solution, expanded_nodes = find_path_astar(graph, start, goals[0])
-        # #print(solution)
-        # #print(expanded_nodes)
-        # file_name = './Outputs/1dot/Astar/' + name + '.txt'
-        # print(file_name)
-        # print_solved_maze(start, maze, solution, goals, expanded_nodes, file_name)
+        start_time=time.time()
+        solution, expanded_nodes = neil_find_path_astar(graph, start, goals)
+        end_time=time.time()
+        print(end_time-start_time)
+        print(solution)
+        print(expanded_nodes)
+        #file_name = './Outputs/1dot/Astar/' + name + '.txt'
+        file_name = './Outputs/Search/Astar/' + name + '.txt'
+        print(file_name)
+        print_solved_maze(start, maze, solution, goals, expanded_nodes, file_name)
         #
         # solution, expanded_nodes = find_path_greedy(graph, start, goals[0])
         # # print(solution)
